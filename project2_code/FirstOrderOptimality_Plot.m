a9de7d8769d2ead38c5d748f0e6c6d1a20b175fa
Kevin Morrison <37518844+KevinMorrison-629@users.noreply.github.com>
2021-08-08 15:00:24 -0400

    Add files via upload

---
+++ project2_code/FirstOrderOptimality_Plot.m
@@ -0,0 +1,12 @@
+
+
+optimality = [9.425e+01, 4.640e+02, 1.504e+02, 2.856e+01, 1.774e+01, 3.822e+01, 3.479e+01, 9.116e+00, 2.177e+00, 8.998e-02, 5.484e-06];
+
+figure(1)
+semilogy(optimality)
+title('First-Order Optimality')
+xlabel('Iteration')
+ylabel('First-Order Optimality')
+
+
+
