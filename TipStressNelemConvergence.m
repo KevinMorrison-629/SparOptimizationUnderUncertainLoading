461c595bcd83ee2c54093dc9a6c2f2dfcb60ea3d
Kevin Morrison <37518844+KevinMorrison-629@users.noreply.github.com>
2021-08-08 14:59:40 -0400

    Delete TipStressNelemConvergence.m

--- TipStressNelemConvergence.m
+++
@@ -1,7 +0,0 @@
-
-Nelems = [10,20,30,40,50];
-figure(2)
-plot(Nelems,abs(real(data_tip_stress(1:5)))/abs(real(mean_stress(1))),'-o')
-title('Effect of {Nelem} on Root-Normalized Tip Stress')
-xlabel('Number of Spar Elements')
-ylabel('Root Normalized Tip Stress')
